922a58cc3e801b75b8973f3b98d3a5886d378090
Marlon <marlonborup@gmail.com>
2015-02-07 12:24:03 -0700

    I got past all the error codes except the com_fonix_ASR_JNICore_FNX_ERR_BUFFER_TOO_SMALL from the getFirstResult call

--- app/src/main/java/com/demo/speechfx/speechfxdemo/activity/AudioRecordActivity.java
+++ app/src/main/java/com/demo/speechfx/speechfxdemo/activity/AudioRecordActivity.java
@@ -1,6 +1,7 @@
 package com.demo.speechfx.speechfxdemo.activity;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
@@ -8,13 +9,36 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Toast;
 
 import com.demo.speechfx.speechfxdemo.R;
+import com.speechfxinc.voicein.android.FnxCore;
+import com.speechfxinc.voicein.android.FnxMemFileMapping;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Formatter;
+
+
+import static com.speechfxinc.voicein.android.FonixCore.create;
+import static com.speechfxinc.voicein.android.FonixCore.createWordNodeEx;
+import static com.speechfxinc.voicein.android.FonixCore.destroy;
+import static com.speechfxinc.voicein.android.FonixCore.getFirstResult;
+import static com.speechfxinc.voicein.android.FonixCore.setAsrDictionaryFile;
+import static com.speechfxinc.voicein.android.FonixCore.setGeneralNNetFile;
+import static com.speechfxinc.voicein.android.FonixCore.setNodeActivePercent;
+import static com.speechfxinc.voicein.android.FonixCore.setNodeRawAudioInput;
+import static com.speechfxinc.voicein.android.FonixCore.setRejectionStrength;
+import static com.speechfxinc.voicein.android.FonixCore.setReplayInput;
+import static com.speechfxinc.voicein.android.FonixCore.setSharedPath;
+import static com.speechfxinc.voicein.android.FonixCoreConstants.MAX_SPEED_FRAME_SIZE_15;
+import static com.speechfxinc.voicein.android.FonixCoreConstants.MAX_SPEED_PRUNE_NNET;
+import static com.speechfxinc.voicein.android.FonixPublic.combineMemFileMaps;
+import static com.speechfxinc.voicein.android.FonixPublic.memFileRegister;
+import static com.speechfxinc.voicein.android.FonixPublic.memFileUnregister;
 
 //    sampleRate: 16000
 //    ENCODING_PCM_16BIT
@@ -23,22 +47,132 @@ import java.io.IOException;
 
 public class AudioRecordActivity extends Activity {
   private static final String TAG = AudioRecordActivity.class.getSimpleName();
-  private static final int RECORDER_SAMPLE_RATE = 16000;
+  private static final String SHARED_PATH = "";
+  private static final String ASR_DICTIONARY = "Colors.pdc";  /* A pared down user dictionary that contains color words. */
+  private static final String WAVE_FILE = "green8k.wav"; /* An audio file containing the word "green". */
+  private static final String GENERAL_NNET = "usgp08FN3108.psi"; /* The neural net file. */
+  private static final String GENERAL_NNET_DESCR = GENERAL_NNET.substring(0, GENERAL_NNET.lastIndexOf('.'));
+
+  private static final int RECORDER_SAMPLE_RATE = 8000;
   private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
   private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
-  private static final int BUFFER_ELEMENTS_2_REC = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
+  private static final int BUFFER_ELEMENTS_2_REC = 1024 * 20; // want to play 2048 (2K) since 2 bytes we use only 1024
   private static final int BYTES_PER_ELEMENT = 2; // 2 bytes in 16bit format
   private AudioRecord recorder = null;
   private Thread recordingThread = null;
   private boolean isRecording = false;
+  ArrayList<FnxMemFileMapping> memFileMappings;
+
+  /*
+   * On Android, an application doesn't generally have direct access to the filesystem.
+   * So the files are read from assets into a FnxMemFileMapping[].  The FnxMemFileMapping[] is
+   * a mock filesystem in memory where each element acts like a File.  If for some reason
+   * you do have access to the filesystem and can read neural nets and dictionaries
+   * from there, you can use File objects, and bypass making a FnxMemFileMapping[].
+   */
+  private ArrayList<FnxMemFileMapping> unpackAssets(AssetManager assetManager) {
+    ArrayList<FnxMemFileMapping> memFileMappings = new ArrayList<FnxMemFileMapping>();
+    ArrayList<Byte> byteList = new ArrayList<Byte>();
+    byte buffer[] = new byte[256];
+    String files[] = new String[]{ASR_DICTIONARY, WAVE_FILE, GENERAL_NNET};
+      /* Read out each file in the asset and allocate a FnxMemFileMapping object.
+       * This is so that data can be read out of the mock files in the filesystem.
+       */
+    for (String filename : files) {
+      Byte[] byteArray;
+      try {
+        InputStream str = assetManager.open(filename);
+        int bytesRead;
+        while ((bytesRead = str.read(buffer)) != -1) {
+          byteArray = new Byte[bytesRead];
+          for (int i = 0; i < bytesRead; ++i) {
+            byteArray[i] = buffer[i];
+          }
+          byteList.addAll(Arrays.asList(byteArray));
+        }
+        str.close();
+      } catch (IOException e) {
+        Log.e(TAG, e.getMessage(), e);
+      }
+      byteArray = byteList.toArray(new Byte[byteList.size()]);
+      byte[] newByteArray = new byte[byteArray.length];
+      for (int i = 0; i < byteArray.length; ++i) {
+        newByteArray[i] = byteArray[i];
+      }
+      memFileMappings.add(new FnxMemFileMapping(filename, newByteArray));
+      byteList.clear();
+    }
+    return memFileMappings;
+  }
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.some_layout);
+    /* read the neural net, .pdc file, and audio */
+    memFileMappings = unpackAssets(getAssets());
     setButtonHandlers();
     enableButtons(false);
 //    int bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLE_RATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
+  }
+
+  private String getMessage(short[] sData) {
+    String message;
+        /* N.B. You might not want to do it in exactly this fashion for a real application,
+         at least with respect to how the UI gets set up. */
+    FnxCore fnxCore = create();
+    if (fnxCore != null) {
+      String recognizedWord[] = new String[1];                            // Buffer to contain recognized word
+              /* You must always use FnxCombineMemFileMaps and FnxMemFileRegister
+                 when working with FnxMemFileMapping[].  FnxCombineMemFileMaps makes a
+                 contiguous array of structs in the JNI layer for the mock filesystem.
+                 This array is the returned ByteBuffer */
+      ByteBuffer buffer = combineMemFileMaps(memFileMappings);
+      memFileRegister(buffer);  // Now the mock files are visible to other Fnx* function calls.
+
+      Log.d(TAG, "setSharedPath " + setSharedPath(fnxCore, new File(SHARED_PATH)));  // Set shared path to location of neural nets, dictionaries, etc.
+      Log.d(TAG, "setReplayInput " + setReplayInput(fnxCore, false)); // replay does not work yet...
+      Log.d(TAG, "setRejectionStrength " + setRejectionStrength(fnxCore, 40));
+
+      int generalNNetID = setGeneralNNetFile(fnxCore, GENERAL_NNET_DESCR, MAX_SPEED_PRUNE_NNET | MAX_SPEED_FRAME_SIZE_15);  // set the neural net
+      Log.d(TAG, "setGeneralNNetFile " + generalNNetID);
+      int nRet = setAsrDictionaryFile(fnxCore, new File(ASR_DICTIONARY));  // set the dictionary
+      Log.d(TAG, "setAsrDictionaryFile " + nRet);
+      int wordNodeID = createWordNodeEx(fnxCore, new String[]{"red", "green", "blue"}, null, false, null, generalNNetID);  // create a word node
+      Log.d(TAG, "createWordNodeEx " + wordNodeID);
+      Log.d(TAG, "setNodeActivePercent " + setNodeActivePercent(fnxCore, wordNodeID, 30));  //used to increase the speed of recognition
+
+      Formatter formatter = new Formatter(new StringBuilder());
+
+      // set the input to the list of waves
+//      if ((nRet = setWaveInput(fnxCore, new File[]{new File(WAVE_FILE)})) < 0) {  // set audio input to the file
+//        formatter.format("setWaveInput error %d\n", nRet);
+//        message = formatter.toString();
+//      }
+      nRet = setNodeRawAudioInput(fnxCore, 0, sData, sData.length);
+      Log.d(TAG, "setNodeRawAudioInput " + nRet);
+//      if ((nRet = runNode(fnxCore, wordNodeID, null)) < 0) {  // process the audio input
+//        formatter.format("runNode error %d\n", nRet);
+//        message = formatter.toString();
+//      }
+
+      nRet = getFirstResult(fnxCore, recognizedWord, null, null, null, null, null); // get recognition results
+      Log.d(TAG, "getFirstResult " + nRet);
+
+      if (recognizedWord[0] != null) {
+        formatter.format("You said, %s", recognizedWord[0]);
+        message = formatter.toString();
+      } else {
+        message = "No word was recognized.";
+      }
+
+      destroy(fnxCore); // clean up the FnxCore object
+      memFileUnregister(buffer); // unregister the memory buffer
+      formatter.close();
+    } else {
+      message = "create() failed";
+    }
+    return message;
   }
 
   private void setButtonHandlers() {
@@ -85,37 +219,12 @@ public class AudioRecordActivity extends Activity {
 
   private void writeAudioDataToFile() {
     // Write the output audio in byte
-//    String filePath = Environment.getExternalStorageDirectory().getPath() + "voice8K16bitmono.pcm";
-    String filePath = "/sdcard/voice8K16bitmono.pcm";
     short sData[] = new short[BUFFER_ELEMENTS_2_REC];
-    FileOutputStream stream = null;
-    try {
-      stream = new FileOutputStream(filePath);
-      while (isRecording) {
-        // gets the voice output from microphone to byte format
-        int count = recorder.read(sData, 0, BUFFER_ELEMENTS_2_REC);
-        Log.i(TAG, count + " bytes read");
-        try {
-          // writes the data to file from buffer
-          // stores the voice buffer
-          byte bData[] = short2byte(sData);
-          stream.write(bData, 0, BUFFER_ELEMENTS_2_REC * BYTES_PER_ELEMENT);
-        } catch (IOException e) {
-          Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-          Log.e(TAG, e.getMessage(), e);
-        }
-      }
-    } catch (FileNotFoundException e) {
-      Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-      Log.e(TAG, e.getMessage(), e);
-    }
-    try {
-      if (stream != null) {
-        stream.close();
-      }
-    } catch (IOException e) {
-      Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-      Log.e(TAG, e.getMessage(), e);
+    while (isRecording) {
+      // gets the voice output from microphone to byte format
+      int count = recorder.read(sData, 0, BUFFER_ELEMENTS_2_REC);
+      Log.i(TAG, count + " bytes read");
+      Log.i(TAG, "getMessage " + getMessage(sData));
     }
   }
 
